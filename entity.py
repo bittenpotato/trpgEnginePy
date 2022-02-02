b0233cbc705267957f3d0f6240eed3afa34ba5cf
suitepotato <suitepotato@outlook.kr>
2022-02-03 01:44:22 +0900

    엔티티 모듈 1차적 작업. devNote.md 확인

--- entity.py
+++ entity.py
@@ -1,5 +1,16 @@
+import math
+
+# TODO
+# 엔티티 클래스
+#	1. 메소드
+#		- giveDamage : 객체가 피해량 받는 함수
+#		- giveHeal : 객체가 회복량 받는 함수
+#		- giveExp : 객체가 경험치 받는 함수
+# 몬스터 클래스
+# 플레이어 클래스
+
 class Entity:
-	def __init__(self, name, level, exp, maxHp, hp, generalAttack, generalDefence, skillPoint=0):
+	def __init__(self, name, level, exp, maxHp, hp, generalAttack, generalDefence):
 		self.name = name
 		self.level = level
 		self.exp = exp
@@ -7,17 +18,33 @@ class Entity:
 		self.hp = hp
 		self.generalAttack = generalAttack
 		self.generalDefence = generalDefence
-		self.skillPoint = skillPoint
 	
 	def getEntityData(self):
+		print("==================================================================")
 		print("name : " + self.name + " Lv." + self.level)
 		print("Hp : " + self.hp + "/" + self.maxHp)
 		print("Exp : " + self.exp)
-		print("")
+		print("ATK : " + self.generalAttack)
+		print("==================================================================")
 
 	def isFainted(self):
 		(self.hp <=0) if True else False
 
-	def attack(self, victim):
-		if (self.generalAttack <= victim.generalDefence):
-			print(self.name + "의 공격!")
+#	def useGeneralAttack(self, victim):
+#		damage = math.round( ( self.generalAttack * ( 1 - ( victim.generalDefence / ( 100 + victim.generalDefence ) ) ) ) )
+#		if (victim.hp < damage):
+#			victim.hp = 0
+#		else:
+#			victim.hp -= damage
+
+#	def useGeneralDefence(self):
+#		self.extraGeneralDefence += ( 0.5 * self.generalDefence )
+#		self.extraDefenceCount = 1
+
+#	def useAttackDamage(self):
+#		return self.generalAttack
+
+#	def getDamage(self, rawDamage):
+		
+	
+#	def useDefence(self):
